dcd2f6251799163a7959aff9ed49943a49fade48
Frans de Boer <frans-de-boer@hotmail.com>
2022-03-07 17:53:30 +0100

    opcode implementations

--- scripts/index.ts
+++ scripts/index.ts
@@ -74,42 +74,125 @@ class CPU {
                 switch (nnn) {
                     case 0x0E0: // 00E0 - CLS
                         this.display.clear();
+                        break;
                     case 0x0EE: // 00EE - RET
                         this.pc = this.stack[this.sp];
                         this.sp = (this.sp - 1) & 0xFF;
+                        break;
                 }
                 break;
             case 0x1: // 1nnn - JP addr
                 this.pc = nnn;
-            case 0x2: // 2nnn - CALL  addr
+                break;
+            case 0x2: // 2nnn - CALL addr
                 this.sp = (this.sp + 1) & 0xFF;
                 this.stack[this.sp] = this.pc;
                 this.pc = nnn;
-            case 0x3:
                 break;
-            case 0x4:
+            case 0x3: // 3xkk - SE Vx, byte
+                if (this.registers[x] == kk) {
+                    this.increasePC();
+                }
                 break;
-            case 0x5:
+            case 0x4: // 4xkk - SNE Vx, byte
+                if (this.registers[x] != kk) {
+                    this.increasePC();
+                }
                 break;
-            case 0x6:
+            case 0x5: // 5xy0 - SE Vx, Vy
+                if (this.registers[x] == this.registers[y]) {
+                    this.increasePC();
+                }
                 break;
-            case 0x7:
+            case 0x6: // 6xkk - LD Vx, byte
+                this.registers[x] = kk;
+                break;
+            case 0x7: // 7xkk - ADD Vx, byte
+                this.registers[x] += kk;
                 break;
             case 0x8:
+                switch (end) {
+                    case 0x0: // 8xy0 - LD Vx, Vy
+                        this.registers[x] = this.registers[y];
+                        break;
+                    case 0x1: // 8xy1 - OR Vx, Vy
+                        this.registers[x] |= this.registers[y];
+                        break;
+                    case 0x2: // 8xy2 - XOR Vx, Vy
+                        this.registers[x] &= this.registers[y];
+                        break;
+                    case 0x3: // 8xy3 - XOR Vx, Vy
+                        this.registers[x] ^= this.registers[y];
+                        break;
+                    case 0x4: // 8xy4 - ADD Vx, Vy
+                        const addResult = this.registers[x] + this.registers[y];
+                        this.registers[x] = addResult;
+                        this.registers[0xF] = (addResult > 255) ? 1 : 0;
+                        break;
+                    case 0x5: // 8xy5 - SUB Vx, Vy
+                        this.registers[0xF] = (this.registers[x] > this.registers[y]) ? 1 : 0;
+                        this.registers[x] = this.registers[x] - this.registers[y];
+                        break;
+                    case 0x6: // 8xy6 - SHR Vx (, Vy)
+                        this.registers[0xF] = this.registers[x] & 0x1;
+                        this.registers[x] >>= 1;
+                        break;
+                    case 0x7: // 8xy7 - SUBN Vx, Vy
+                        this.registers[0xF] = (this.registers[y] > this.registers[x]) ? 1 : 0;
+                        this.registers[x] = this.registers[y] - this.registers[x];
+                        break;
+                    case 0xE: // 8xyE - SHL Vx (, Vy)
+                        this.registers[0xF] = this.registers[x] & 0x80;
+                        this.registers[x] <<= 1;
+                        break;
+                }
                 break;
-            case 0x9:
+            case 0x9: // 9xy0 - SNE Vx, Vy
+                if (this.registers[x] != this.registers[y]) {
+                    this.increasePC();
+                }
                 break;
-            case 0xA:
+            case 0xA: // Annn - LD I, addr
+                this.I = nnn;
                 break;
-            case 0xB:
+            case 0xB: // Bnnn - JP V0, addr
+                this.I = nnn + this.registers[0];
                 break;
-            case 0xC:
+            case 0xC: // Cxkk - RND Vx, byte
+                const rnd = Math.floor(Math.random() * 256);
+                this.registers[x] = rnd & kk
                 break;
-            case 0xD:
+            case 0xD: // Dyxn - DRW Vx, Vy, nibble
                 break;
             case 0xE:
+                switch (kk) {
+                    case 0x9E: // Ex9E - SKP Vx
+                        break;
+                    case 0xA1: // ExA1 - SKNP Vx
+                        break;
+                }
                 break;
             case 0xF:
+                switch (kk) {
+                    case 0x07:
+                        break;
+                    case 0x0A:
+                        break;
+                    case 0x15:
+                        break;
+                    case 0x18:
+                        break;
+                    case 0x1E:
+                        break;
+                    case 0x29:
+                        break;
+                    case 0x33:
+                        break;
+                    case 0x55:
+                        break;
+                    case 0x65:
+                        break;
+                }
                 break;
         }
     }
