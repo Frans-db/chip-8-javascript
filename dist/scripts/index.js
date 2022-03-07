a9aa7049c906f2a3d57aad92f3034321f4bdfcee
Frans de Boer <frans-de-boer@hotmail.com>
2022-03-07 19:34:05 +0100

    more opcodes, created basic display, removed dist from gitignore

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,8 +1,5 @@
+"use strict";
 class Display {
-    private width: number;
-    private height: number;
-    private display: number[][];
-
     constructor() {
         this.width = 64;
         this.height = 32;
@@ -12,7 +9,6 @@ class Display {
             this.display[i].fill(0);
         }
     }
-
     clear() {
         this.display = new Array(this.width);
         for (let i = 0; i < this.display.length; i++) {
@@ -20,57 +16,35 @@ class Display {
             this.display[i].fill(0);
         }
     }
-
-    drawPixel(x: number, y: number): boolean {
+    drawPixel(x, y) {
         const boundedX = x % this.width;
         const boundedY = y % this.height;
         const collision = this.display[boundedX][boundedY] == 1;
         this.display[boundedX][boundedY] ^= 1;
         return collision;
     }
-
-    getDisplay(): number[][] {
+    getDisplay() {
         return this.display;
     }
 }
-
 class CPU {
-    private memory: Uint8Array;    // 4096 8-bit values
-    private registers: Uint8Array; // 16 8-bit values. 0xF is flag register
-    private stack: Uint16Array;    // 16 16-bit values
-
-    private I: number;  // 16-bit memory address register
-    private pc: number; // 16-bit program counter register
-    private sp: number; // 8-bit stack pointer register
-
-    private dt: number; // 8-bit delay register
-    private st: number; // 8-bit sound register
-
-    private display: Display;
-
     constructor() {
         this.memory = new Uint8Array(4096);
         this.registers = new Uint8Array(16);
         this.stack = new Uint16Array(16);
-
         this.I = 0;
         this.pc = 0x200;
         this.sp = 0;
-
         this.dt = 0;
         this.st = 0;
-
         this.display = new Display();
-
         this.loadSprites();
     }
-
     step() {
         const opcode = this.memory[this.pc] << 8 | this.memory[this.pc + 1];
         this.executeOpcode(opcode);
     }
-
-    executeOpcode(opcode: number) {
+    executeOpcode(opcode) {
         // TODO (Frans): The current way RET and CALL work makes it so the first address of the stack is never used.
         // this is according to the specification, but should this be changed?
         this.increasePC();
@@ -80,7 +54,6 @@ class CPU {
         const y = (opcode & 0x00F0) >> 4;
         const nnn = (opcode & 0x0FFF);
         const kk = (opcode & 0x00FF);
-
         switch (start) {
             case 0x0:
                 switch (nnn) {
@@ -172,7 +145,7 @@ class CPU {
                 break;
             case 0xC: // Cxkk - RND Vx, byte
                 const rnd = Math.floor(Math.random() * 256);
-                this.registers[x] = rnd & kk
+                this.registers[x] = rnd & kk;
                 break;
             case 0xD: // Dxyn - DRW Vx, Vy, nibble
                 let collision = false;
@@ -187,7 +160,7 @@ class CPU {
                         }
                         value <<= 1;
                     }
-                    this.registers[0xF] = collision ? 1 : 0; 
+                    this.registers[0xF] = collision ? 1 : 0;
                 }
                 break;
             case 0xE:
@@ -219,9 +192,9 @@ class CPU {
                         this.I = this.registers[x] * 5;
                         break;
                     case 0x33: // Fx33 - LD B, Vx
-                        this.memory[this.I    ] = Math.floor(this.registers[x] / 100);
-                        this.memory[this.I + 1] = Math.floor((this.registers[x] % 100) / 10)
-                        this.memory[this.I + 2] = Math.floor(this.registers[x] % 10)
+                        this.memory[this.I] = Math.floor(this.registers[x] / 100);
+                        this.memory[this.I + 1] = Math.floor((this.registers[x] % 100) / 10);
+                        this.memory[this.I + 2] = Math.floor(this.registers[x] % 10);
                         break;
                     case 0x55: // Fx55 - LD [I], Vx
                         for (let i = 0; i <= x; i++) {
@@ -237,52 +210,46 @@ class CPU {
                 break;
         }
     }
-
     increasePC() {
         this.pc += 2;
         this.pc = this.pc & 0xFFFF;
     }
-
-    loadProgram(program: Uint8Array) {
+    loadProgram(program) {
         for (let i = 0; i < program.length; i++) {
             this.memory[0x200 + i] = program[i];
         }
     }
-
-    getDisplay(): number[][] {
+    getDisplay() {
         return this.display.getDisplay();
     }
-
     loadSprites() {
         const sprites = [
-            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-            0x20, 0x60, 0x20, 0x20, 0x70, // 1
-            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+            0xF0, 0x90, 0x90, 0x90, 0xF0,
+            0x20, 0x60, 0x20, 0x20, 0x70,
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,
+            0x90, 0x90, 0xF0, 0x10, 0x10,
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,
+            0xF0, 0x10, 0x20, 0x40, 0x40,
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,
+            0xF0, 0x90, 0xF0, 0x10, 0xF0,
+            0xF0, 0x90, 0xF0, 0x90, 0x90,
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,
+            0xF0, 0x80, 0x80, 0x80, 0xF0,
+            0xE0, 0x90, 0x90, 0x90, 0xE0,
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,
+            0xF0, 0x80, 0xF0, 0x80, 0x80 // F
         ];
         for (let i = 0; i < sprites.length; i++) {
             this.memory[i] = sprites[i];
         }
     }
 }
-
 function step() {
     if (!ctx) {
         return;
     }
-
     cpu.step();
     ctx.clearRect(0, 0, 200, 200);
     // render frame
@@ -293,7 +260,8 @@ function step() {
         for (let j = 0; j < col.length; j++) {
             if (col[j]) {
                 ctx.fillStyle = 'black';
-            } else {
+            }
+            else {
                 ctx.fillStyle = 'white';
             }
             ctx.fillRect(i * size, j * size, size, size);
@@ -302,28 +270,24 @@ function step() {
     }
     window.requestAnimationFrame(step);
 }
-
-const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const cpu = new CPU();
-let program: Uint8Array;
-
-
-function showFile(input: HTMLInputElement) {
+let program;
+function showFile(input) {
     if (!input.files) {
         return;
     }
     const file = input.files[0];
     const reader = new FileReader();
-
-    reader.onload = function() {
-        const result = reader.result as string
+    reader.onload = function () {
+        const result = reader.result;
         program = new Uint8Array(result.length);
         for (let i = 0; i < result.length; i++) {
             program[i] = result.charCodeAt(i);
         }
         cpu.loadProgram(program);
         window.requestAnimationFrame(step);
-    }
+    };
     reader.readAsBinaryString(file);
 }
